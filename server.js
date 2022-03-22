389ff9e37844764cf4bd11f33afd8cc7585976ad
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-22 20:53:28 +0100

    march 22th

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+a
 
 app.listen(PORT);
 
