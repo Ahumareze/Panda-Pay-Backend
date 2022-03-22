31459e6d8f459091341784e1e6c633e219481901
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-22 13:04:57 +0100

    march 22th

--- server.js
+++ server.js
@@ -9,63 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const transport = {
-    host: 'smtp.gmail.com',
-    auth: {
-        user: 'pandamailingservice@gmail.com',
-        pass: 'de51gner'
-    }
-};
+const users = [
+  {username: 'king', email: 'king@gmail.com', id: 0, balance: 100},
+  {username: 'martin', email: 'martin@gmail.com', id: 1, balance: 200},
+  {username: 'sarah', email: 'sarah@gmail.com', id: 2, balance: 300},
+]
 
-const transporter = nodemailer.createTransport(transport);
+app.get('/', (req, res) => {
+  res.send(users)
+});
 
-transporter.verify((err, suss) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(suss + ' Server is ready to take messages')
-    }
-})
-
-// app.get('/msg', (req, res) => {
-//     var mail = {
-//         from: 'panda',
-//         to: 'ahumarezeifeanyi001@gmail.com',  //Change to email address that you want to receive messages on
-//         subject: 'Pool party request',
-//         text: 'Helo there wanna come to my pool?'
-//     }
-//     transporter.sendMail(mail, (err, data) => {
-//         if (err) {
-//           res.json({
-//             msg: 'fail'
-//           })
-//         } else {
-//           res.json({
-//             msg: 'success ' + data
-//           })
-//         }
-//     })
-// })
-
-app.post('/sendmessage', (req, res) => {
-    const data = (req.body);
-    var mail = {
-        from: data.name,
-        subject: data.title,
-        text: data.content,
-        to: data.emails
-    };
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {
-          res.json({
-            msg: 'fail'
-          })
-        } else {
-          res.json({
-            msg: 'success ' + data
-          })
-        }
-    })
-})
+app.get('/user', (req, res) => {
+  const username  = req.body.username;
+  res.send(username)
+});
 
 app.listen(PORT)
